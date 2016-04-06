a175f4432c8dbb7accf57b5bb0f0f577bae05bdb
dyong <dyong525@163.com>
2016-04-06 17:01:48 +0800

    update

--- sysmaven-dao/src/main/java/com/sys/dao/mybatis/MyBatisRepository.java
+++ sysmaven-dao/src/main/java/com/sys/dao/mybatis/MyBatisRepository.java
@@ -1,12 +1,13 @@
 package com.sys.dao.mybatis;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
 import org.springframework.stereotype.Component;
 
-import java.lang.annotation.*;
-
-/**
- * Created by fanxiyue on 15/7/6.
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Documented
